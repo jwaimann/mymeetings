2ed84f4a5ce73022e295744867644567d0222d68
jwaimann <jwaimann@makingsense.com>
2015-06-30 10:59:25 -0300

    Update testing commit

--- public/javascripts/mymeetingsApp.js
+++ public/javascripts/mymeetingsApp.js
@@ -11,7 +11,6 @@ var app = angular.module('mymeetingsApp', ['ngRoute', 'ngResource', 'luegg.direc
   }; 
 });
 
-
 app.config(function($routeProvider){
   $routeProvider
     .when('/meetings', {
@@ -31,7 +30,6 @@ app.config(function($routeProvider){
       controller: 'authController'
     });
 });
-
 
 app.factory('postService', function($resource){
   return $resource('/api/posts/:id');
